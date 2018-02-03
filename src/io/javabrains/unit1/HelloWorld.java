bffafbcc62a7918b7733d8bdb5dd32f357f8a6a6
neeraj7836 <ng9354@gmail.com>
2018-02-03 14:40:19 +0530

    New sys.out

--- src/io/javabrains/unit1/HelloWorld.java
+++ src/io/javabrains/unit1/HelloWorld.java
@@ -5,6 +5,7 @@ public static void main(String[] args)
 {
 System.out.println("Hello, World!!!");
 System.out.println("My name is neeraj");
+System.out.println("I am doing dev-ops training");
 }
 }
 
